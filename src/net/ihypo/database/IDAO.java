f599010cd1475207a111901d1753efa528f6f629
Coderhypo <i@ihypo.net>
2015-12-26 23:13:21 +0800

    update new file

---
+++ src/net/ihypo/database/IDAO.java
@@ -0,0 +1,7 @@
+package net.ihypo.database;
+
+/**
+ * Created by hypo on 15-12-26.
+ */
+public interface IDAO {
+}
---
+++ src/net/ihypo/test/tools/TestPassTool.java
@@ -0,0 +1,7 @@
+package net.ihypo.test.tools;
+
+/**
+ * Created by hypo on 15-12-26.
+ */
+public class TestPassTool {
+}
