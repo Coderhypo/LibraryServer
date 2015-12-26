751f0ff82922e804c5b37546c828f1920e8a805b
Coderhypo <i@ihypo.net>
2015-12-26 23:16:21 +0800

    test TestToken

--- src/net/ihypo/test/tools/TestToken.java
+++ src/net/ihypo/test/tools/TestToken.java
@@ -1,7 +1,27 @@
 package net.ihypo.test.tools;
 
+import net.ihypo.models.Manager;
+import net.ihypo.tools.TokenTool;
+import net.ihypo.user.IUser;
+
 /**
  * Created by hypo on 15-12-26.
  */
 public class TestToken {
+
+    private static IUser getUser(){
+
+        IUser rnt = new Manager("LOGIN", "NAME", "PASSWORD");
+
+        return rnt;
+    }
+
+    public static void main(String[] args){
+
+        int n = 10;
+        while(n-- != 0){
+
+            System.out.println(TokenTool.getToken(getUser()));
+        }
+    }
 }
