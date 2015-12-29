74e99ad987923b1c241683b10166bafaf26b38cf
Coderhypo <i@ihypo.net>
2015-12-29 11:01:41 +0800

    create book acthon

--- src/net/ihypo/actions/BookAction.java
+++ src/net/ihypo/actions/BookAction.java
@@ -1,7 +1,40 @@
 package net.ihypo.actions;
 
+import com.opensymphony.xwork2.ActionSupport;
+
+import java.util.Map;
+
 /**
+ * 提供图书增删改查的API
  * Created by hypo on 15-12-29.
  */
-public class BookAction {
+public class BookAction extends ActionSupport {
+
+    /** 作为API的返回结果的返回值 */
+    private Map json;
+
+    /** 添加图书API实现 */
+    public String addBook(){
+
+        return SUCCESS;
+    }
+
+    /** 删除图书API实现 */
+    public String deleteBook(){
+
+        return SUCCESS;
+    }
+
+    /** 更新图书API实现 */
+    public String updateBook(){
+
+        return SUCCESS;
+    }
+
+    /** 查询图书API实现 */
+    public String getBook(){
+
+        return SUCCESS;
+    }
+
 }
