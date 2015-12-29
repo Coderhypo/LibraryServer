9411a049561c267a27f5023d5cad9ff1c93a2797
Coderhypo <i@ihypo.net>
2015-12-29 11:11:23 +0800

    add not find error

--- src/net/ihypo/actions/ErrorAction.java
+++ src/net/ihypo/actions/ErrorAction.java
@@ -1,7 +1,29 @@
 package net.ihypo.actions;
 
+import com.opensymphony.xwork2.ActionSupport;
+
+import java.util.Map;
+
 /**
+ * 提供错误使用API时的反馈
  * Created by hypo on 15-12-29.
  */
-public class ErrorAction {
+public class ErrorAction extends ActionSupport{
+
+    /** 作为API的返回结果的返回值 */
+    private Map json;
+
+    /** 使用API的URI错误NOT FIND */
+    public String notFind(){
+
+        return SUCCESS;
+    }
+
+    public Map getJson() {
+        return json;
+    }
+
+    public void setJson(Map json) {
+        this.json = json;
+    }
 }
