a62f13bb7180447d7821260eb05bf1918a3eb3d7
Coderhypo <i@ihypo.net>
2016-01-04 16:19:28 +0800

    create OperatorAction.java

--- src/net/ihypo/actions/OperatorAction.java
+++ src/net/ihypo/actions/OperatorAction.java
@@ -1,7 +1,35 @@
 package net.ihypo.actions;
 
+import com.opensymphony.xwork2.ActionSupport;
+
+import java.util.Map;
+
 /**
+ * 借阅图书，归还图书相关操作
  * Created by hypo on 16-1-4.
  */
-public class OperatorAction {
+public class OperatorAction extends ActionSupport {
+
+    /** 作为API的返回结果的返回值 */
+    private Map json;
+
+    /** 借阅图书API的实现 */
+    public String lendBook(){
+
+        return SUCCESS;
+    }
+
+    /** 归还图书API的实现 */
+    public String returnBook(){
+
+        return SUCCESS;
+    }
+
+    public Map getJson() {
+        return json;
+    }
+
+    public void setJson(Map json) {
+        this.json = json;
+    }
 }
